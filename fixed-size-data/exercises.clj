aeb1af2b06f8ea6c07cf8f64c91dfa9caa5a9461
apktntaj <maiaelarsya@gmail.com>
2025-06-07 17:32:20 +0700

    Do exercise in clojure

---
+++ fixed-size-data/exercises.clj
@@ -0,0 +1,34 @@
+
+
+#_{:clj-kondo/ignore [:namespace-name-mismatch]}
+(ns fixed-size-data.exercises)
+
+; EXCERCISE 1.1
+;; Write down an expression that computes the distance of this point to the origin.
+
+(defn distance-to-origin [x y]
+  (Math/sqrt (+ (* x x) (* y y))))
+
+(distance-to-origin 3 4) ; => 5.0
+
+;; EXCERCISE 1.2
+;; Add "_" between two strings.
+(defn underscored [s1 s2]
+  (str s1 "_" s2))
+
+(underscored "hello" "world") ; => "hello_world"
+
+;; EXCERCISE 1.3
+;; create an expression using string primitives that adds "_" at position i
+
+(defn underscored-at [string idx]
+  (str (subs string 0 idx) "_" (subs string idx)))
+
+
+(underscored-at "hello" 2) ; => "he_llo"
+
+;; EXCERCISE 1.4
+;; create an expression using string primitives that adds deleted at position i
+
+(defn delete-at [i s]
+  (str (subs s 0 (dec i)) (subs s i)))
